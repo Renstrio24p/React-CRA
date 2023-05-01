befbd77dbfe473969eaa7de94f566c141600ff20
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-01 15:52:16 +0800

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ReactApps from './react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ReactApps />
+  </React.StrictMode>
+);
+
---
+++ src/react.jsx
@@ -0,0 +1,7 @@
+export default function ReactApps() {
+  return (
+    <div className="app">
+       <h1> Hello </h1>
+    </div>
+  );
+}
